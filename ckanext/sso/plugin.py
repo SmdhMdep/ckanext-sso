a8a8767b557d0a51d5a270597e2f1ac3b163f9ff
Dylan Bennett <52495713+Dylanben144@users.noreply.github.com>
2023-10-06 11:20:39 +0100

    Fix org not being taken off user + comments (#4)
    * Fix org not being taken off user + comments
    
    * Fixed logic on PR comments
    
    ---------
    
    Co-authored-by: Dylan Bennett <dylan.bennett@stfc.ac.uk>

--- ckanext/sso/plugin.py
+++ ckanext/sso/plugin.py
@@ -45,44 +45,60 @@ class SsoPlugin(plugins.SingletonPlugin):
         user_id = str(toolkit.g.userobj.id)
         user = model.User.by_name(username)
 
-        if is_ckan_admin:
+        if is_ckan_admin: # add sysadmin privileges
             user.sysadmin = True
             model.Session.add(user)
             model.Session.commit()
             return resp
+        else: # remove sysadmin privileges
+            user.sysadmin = False
+            model.Session.add(user)
+            model.Session.commit()
+                
+            # get users organisation from saml attributes
+            try:
+                expected_org_title = saml_attributes["member"][0] # use the first organization in the list only
+                expected_org_name = expected_org_title.replace(' ', '-').lower()
+            except (KeyError, IndexError):
+                log.error("%s doesn't have a organisation", username)
+                return resp
+            
+            # Remove from all orgs other than the one they are meant to be in
+            current_orgs = toolkit.get_action("organization_list_for_user")({'ignore_auth': True}, {})
+            IsInOrg = False
+            for org in current_orgs:
+                if org["name"] == expected_org_name: #if user is already in correct org
+                    IsInOrg = True
+                else:
+                    try:
+                        toolkit.get_action("organization_member_delete")({'ignore_auth': True}, {"id": org["name"], "username": user_id})
+                    except:
+                        log.error("Cannot delete from: %s", org["name"])
 
-        # remove sysadmin privileges
-        user.sysadmin = False
-        model.Session.add(user)
-        model.Session.commit()
+            if IsInOrg: #If user is already in the org no need to do processing below
+                return resp
+            
+            # Get all organisations
+            try:
+                organization = toolkit.get_action("organization_show")({'ignore_auth': True}, {"id": expected_org_name})
+            except toolkit.ObjectNotFound:
+                organization = None
 
-        # Check if their organisation exist. If it does, add them to it at assigned level
-        try:
-            expected_org_title = saml_attributes["member"][0] # use the first organization in the list only
-            expected_org_name = expected_org_title.replace(' ', '-').lower()
-        except (KeyError, IndexError):
-            log.error("unexpected user with no group membership: %s", username)
+            # Check if their organisation exist. If it does, add them to it at assigned level
+            try:
+                if organization:
+                    log.info("adding user %s to organization %s", username, organization['name'])
+                    data = {"id" : organization["id"] , "username" : user_id, "role" : user_org_role}
+                    toolkit.get_action("organization_member_create")({'ignore_auth': True}, data)
+                else:
+                    log.info("creating organization %s with user %s", expected_org_name, username)
+                    org_users = [{"name" : user_id, "capacity": user_org_role}]
+                    data = {"name": expected_org_name, "title": expected_org_title, "users": org_users}
+                    toolkit.get_action("organization_create")({'ignore_auth': True}, data)
+            except:
+                log.exception("unable to create user %s organization membership for %s", username, expected_org_name)
+
             return resp
-
-        try:
-            organization = toolkit.get_action("organization_show")({'ignore_auth': True}, {"id": expected_org_name})
-        except toolkit.ObjectNotFound:
-            organization = None
-
-        try:
-            if organization:
-                log.info("adding user %s to organization %s", username, organization['name'])
-                data = {"id" : organization["id"] , "username" : user_id, "role" : user_org_role}
-                toolkit.get_action("organization_member_create")({'ignore_auth': True}, data)
-            else:
-                log.info("creating organization %s with user %s", expected_org_name, username)
-                org_users = [{"name" : user_id, "capacity": user_org_role}]
-                data = {"name": expected_org_name, "title": expected_org_title, "users": org_users}
-                toolkit.get_action("organization_create")({'ignore_auth': True}, data)
-        except:
-            log.exception("unable to create user %s organization membership for %s", username, expected_org_name)
-
-        return resp
 
 
 def has_role(roles, role):
